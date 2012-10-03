234cc5893fe4908cd12b7a41ad1bd502b87ebf76
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-03 21:17:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/amazon-auto-links/trunk@607680 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 1.0.8/classes/AmazonAutoLinks_Widget_.php
@@ -0,0 +1,127 @@
+<?php
+class AmazonAutoLinks_Widget_ extends WP_Widget {
+
+	/*
+		Since: v1.0.6
+		Description: Creates a widget.
+	*/
+	public function __construct() {
+		parent::__construct(
+	 		'amazonautolinks_widget', 
+			'Amazon Auto Links', 
+			array( 'description' => __( 'Amazon Auto Links widget', 'amazonautolinks' ), ) // Args
+		);
+	}
+
+	public function widget( $args, $instance ) {
+		extract( $args );
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		$unit = apply_filters( 'widget_title', $instance['unit'] );	// backward compatibility or v1.0.6 or below
+		$strUnitID = apply_filters( 'widget_title', $instance['strUnitID'] );
+		
+		echo $before_widget;
+		
+		if ( ! empty( $title ) ) echo $before_title . $title . $after_title;
+		
+		/* START WIDGET CODE */
+		$oAALOptions = new AmazonAutoLinks_Options(AMAZONAUTOLINKSKEY);
+		
+		if (isset($instance['unit'])) {	
+			
+			// backward compatiblity v 1.0.6 or below
+			foreach ($oAALOptions->arrOptions['units'] as $strUnitKey => $arrUnitOption) {
+				if ($arrUnitOption['unitlabel'] == $unit || (isset($arrUnitOption['id']) && $arrUnitOption['id'] == $strUnitID)) {
+					$oAAL = new AmazonAutoLinks_Core($oAALOptions->arrOptions['units'][$strUnitKey]);
+// echo 'found the match: ' . $unit . '<br />';								
+					break;
+				}
+			}
+		}
+		else {		// there is no problem	
+			// && !isset($oAALOptions->arrOptions['units'][$strUnitID])
+// echo 'called normally: ' . $strUnitID . '<br />';			
+			$oAAL = new AmazonAutoLinks_Core($oAALOptions->arrOptions['units'][$strUnitID]); // the constructor accepts a unit option in the first parameter
+		}
+		
+		if ($oAAL) echo $oAAL->fetch();
+		
+		/* END of WIDGET CODE */
+		echo $after_widget;
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['strUnitID'] = $new_instance['strUnitID'];
+		$instance['unit'] = ''; //$old_instance['unit'];	// for backward compatiblity v1.0.6 or below, return the old value
+		return $instance;
+	}
+
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) $title = $instance[ 'title' ];
+		else $title = __( 'New title', 'amazonautolinks' );
+		$selected_unit = ( isset( $instance[ 'strUnitID' ] ) ) ? $instance[ 'strUnitID' ] : '';
+		$unit = ( isset( $instance[ 'unit' ] ) && !empty($instance[ 'unit' ]) ) ? $instance[ 'unit' ] : '';	// for backward compatiblity v1.0.6 or below
+		
+		$arrOptions = get_option('amazonautolinks');
+		
+		// for backward compatiblity v 1.0.6 or below, which does not have a unique key in the option
+		$arrOptions = $this->fix_unitoption($arrOptions);
+		
+		// for backward compatiblity v 1.0.6 or below, set the selected unit from the previous version's selected unit label 
+		if (!empty($unit) && empty($selected_unit)) {
+			foreach($arrOptions['units'] as $arrUnitOption) {
+				if ($arrUnitOption['unitlabel'] == $unit) {
+					$selected_unit = $arrUnitOption['id'];
+					break;
+				}
+			}
+		}
+		
+		$numUnits = count($arrOptions['units']);
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'amazonautolinks' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'strUnitID' ); ?>"><?php _e( 'Units:', 'amazonautolinks' ); ?></label><br />
+			<select name="<?php echo $this->get_field_name( 'strUnitID' ); ?>" id="<?php echo $this->get_field_id( 'strUnitID' ); ?>">
+				<option value=""><?php echo $numUnits > 0 ? __('Select Unit', 'amazonautolinks') : __('No Unit' , 'amazonautolinks'); ?></option>
+				<?php 
+				foreach($arrOptions['units'] as $arrUnitOption) {
+					echo '<option value="' . esc_attr( $arrUnitOption['id'] ) . '" ' . ($arrUnitOption['id'] == $selected_unit ? 'selected="Selected"' : '') .  '>'
+						. $arrUnitOption['unitlabel'] 
+						. '</option>';
+					
+					}
+				?>
+			</select>
+			<input type="hidden" name="<?php echo $this->get_field_name( 'unit' ); ?>" value="" />	
+		</p>
+		<?php 
+		/* 
+		<input type="hidden" id="<?php echo $this->get_field_id( 'unit' ); ?>" name="<?php echo $this->get_field_name( 'unit' ); ?>" value="<?php echo esc_attr( $arrUnitOption['unitlabel'] ) ; // for backward compatiblity v1.0.6 or below?>" />			
+		*/
+	}
+	function fix_unitoption($arrOptions) {
+		
+		// since v1.0.7
+		// if there is a unit option without the ID element, adds it and rename the unit key to be the unique ID. 
+		$arrUnitKeys_without_ID = array();
+		foreach($arrOptions['units'] as $strUnitKey => $arrUnitOption) 
+			if (!isset($arrUnitOption['id'])) array_push($arrUnitKeys_without_ID, $strUnitKey);
+		if (count($arrUnitKeys_without_ID) == 0) return $arrOptions;	// no problem found, return
+		
+		// fix the option to have an ID and store it with the key name of the ID
+		foreach($arrUnitKeys_without_ID as $strUnitKey){
+			$arrUnitOption = $arrOptions['units'][$strUnitKey];
+			$arrUnitOption['id'] = uniqid();
+			unset($arrOptions['units'][$strUnitKey]);
+			$arrOptions['units'][$arrUnitOption['id']] = $arrUnitOption;
+		}
+		update_option('amazonautolinks', $arrOptions);
+		return $arrOptions;
+	}
+} 
+
