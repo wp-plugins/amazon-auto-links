bda9ec4c592f7eaaf39122bbba9046e890f9ffc0
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-15 10:32:05 +0000

    Fixed an issue that the information in the footer was missing the framework name.
    git-svn-id: https://plugins.svn.wordpress.org/amazon-auto-links/trunk@822381 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- library/admin-page-framework-for-amazon-auto-links.min.php
+++ library/admin-page-framework-for-amazon-auto-links.min.php
@@ -1,5 +1,13 @@
 <?php
 /*
+	Library Name: Admin Page Framework
+	Library URI: http://wordpress.org/extend/plugins/admin-page-framework/
+	Author:  Michael Uno
+	Author URI: http://michaeluno.jp
+	Version: 2.1.6
+	Requirements: WordPress 3.3 or above, PHP 5.2.4 or above.
+	Description: Provides simpler means of building administration pages for plugin and theme developers.
+
 Admin Page Framework - A PHP library that enables to create WordPress admin pages
 Copyright (C) 2013 Michael Uno
 
