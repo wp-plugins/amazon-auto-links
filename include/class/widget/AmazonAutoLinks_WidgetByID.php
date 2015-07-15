fc5875ad4b739ff0d122548893bbd9cd4e7bff90
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-15 10:20:32 +0000

    Uploading 3.1.0b03.
    git-svn-id: https://plugins.svn.wordpress.org/amazon-auto-links/trunk@1199260 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/class/widget/AmazonAutoLinks_WidgetByID.php
+++ include/class/widget/AmazonAutoLinks_WidgetByID.php
@@ -10,7 +10,7 @@
 /**
  * Creates a widget by unit.
  * 
- * @since   3
+ * @since       3
  * @remark      The class name is important as it is used as the widget ID. 
  * And the name `AmazonAutoLinks_WidgetByID` is also used as the widget ID in v2.
  */
@@ -75,11 +75,27 @@ class AmazonAutoLinks_WidgetByID extends AmazonAutoLinks_AdminPageFramework_Widg
          * @since       3.0.5
          * @return      void
          */
-        private function _addFieldsByFieldClass( $aClassNames ) {     
+        private function _addFieldsByFieldClass( $aClassNames ) {    
+        
             foreach( $aClassNames as $_sClsssName ) {            
                 $_oFields = new $_sClsssName;
                 foreach( $_oFields->get() as $_aField ) {
+                    
+                    $_aField = $_aField + array(
+                       'field_id' => null,
+                    );
+                    
+                    // Modify a field 
+                    if ( 'available_page_types' === $_aField[ 'field_id' ] ) {
+                        $_aField[ 'default' ] = array(
+                            'home'              => true,
+                            'front'             => true,
+                        ) + $_aField[ 'default' ];
+                    }
+                    
+                    // Add the iterating field
                     $this->addSettingFields( $_aField );
+                    
                 }
             }            
         }
@@ -147,6 +163,8 @@ class AmazonAutoLinks_WidgetByID extends AmazonAutoLinks_AdminPageFramework_Widg
                 'height'                    => 400,
                 'height_unit'               => 'px',
                 'available_page_types'      => array(
+                    'home'              => true,
+                    'front'             => true,
                     'singular'          => true,
                     'post_type_archive' => false,
                     'taxonomy'          => false,
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:        http://en.michaeluno.jp/donate
 Tags:               amazon, link, links, ad, ads, advertisement, widget, widgets, sidebar, post, posts, affiliate, affiliate marketing, ecommerce, internet-marketing, marketing, monetization, revenue, shortcode
 Requires at least:  3.3
 Tested up to:       4.2.2
-Stable tag:         3.0.5
+Stable tag:         3.0.4
 License:            GPLv2 or later
 License URI:        http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -172,6 +172,11 @@ You can check if your access key is valid or not with [Scratchpad](http://associ
 4. **Widget Sample**
 
 == Changelog ==
+
+= 3.1.0 =
+- Added the ability to produce RSS and JSON feeds by unit id.
+- Added the home and front page criteria for the `Available Page Types` option in the widget form.
+- Fixed an issue that widget by unit could not be displayed in the front/home page, introduced in 3.0.5.
 
 = 3.0.5 - 07/14/2015 =
 - Added visibility options to the widget by unit.
